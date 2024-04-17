65d0feac450ffbfb118441458ce75987ca9548da
kokos423 <sasa.azra2002@gmail.com>
2024-04-17 22:59:29 +0200

    Update main.dev.js

--- assets/javascript/dist/main.dev.js
+++ assets/javascript/dist/main.dev.js
@@ -10,4 +10,3 @@ for (var i = 0; i < linkovi.length; i++) {
 
 meni += '<li class="nav-item"><a href="#" class="nav-link"> Documentation </a></li>';
 meni += "</ul>";
-document.querySelector("#headerlista").innerHTML = meni;
